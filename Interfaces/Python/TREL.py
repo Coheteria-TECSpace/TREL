fb69a0a3c3ec0d53483400797237b5f0f54e9220
lross2k <luisross2000@outlook.com>
2023-04-25 20:40:07 -0600

    It's done
    ~lross2k~

--- Interfaces/Python/TREL.py
+++ Interfaces/Python/TREL.py
@@ -141,7 +141,7 @@ class TREL_HEIGHT_SIM(ctypes.Structure):
                 ("max_drag", ctypes.c_double)]
 
 # Struct and constructor for rocket
-class TREL_ROCKET(ctypes.Structure):
+class _TREL_ROCKET_T(ctypes.Structure):
     _fields_ = [("engine", ctypes.POINTER(ctypes.POINTER((_TREL_ENGINE_T)))),
                 ("telemetry_mass", ctypes.c_double),
                 ("parachute_mass", ctypes.c_double),
@@ -157,8 +157,8 @@ class TREL_ROCKET(ctypes.Structure):
                 ("drag_coefficient", ctypes.c_double),
                 ("sim_values", ctypes.POINTER(TREL_HEIGHT_SIM))]
 
-trel_rocket_init = TREL.trel_rocket_init
-trel_rocket_init.argtypes = [ctypes.POINTER(ctypes.POINTER((_TREL_ENGINE_T))),
+_trel_rocket_init = TREL.trel_rocket_init
+_trel_rocket_init.argtypes = [ctypes.POINTER(ctypes.POINTER((_TREL_ENGINE_T))),
                              ctypes.c_double,
                              ctypes.c_double,
                              ctypes.c_double,
@@ -167,16 +167,16 @@ trel_rocket_init.argtypes = [ctypes.POINTER(ctypes.POINTER((_TREL_ENGINE_T))),
                              ctypes.c_double,
                              ctypes.c_double,
                              ctypes.c_double]
-trel_rocket_init.restype = ctypes.c_void_p
+_trel_rocket_init.restype = ctypes.c_void_p
 
 trel_run_area_comp_iterations = TREL.trel_run_area_comp_iterations
 trel_run_area_comp_iterations.argtypes = [ctypes.POINTER(ctypes.POINTER((_TREL_ENGINE_T)))]
 
 trel_run_time_comp_iterations = TREL.trel_run_time_comp_iterations
-trel_run_time_comp_iterations.argtypes = [ctypes.POINTER(ctypes.POINTER(TREL_ROCKET))]
+trel_run_time_comp_iterations.argtypes = [ctypes.POINTER(ctypes.POINTER(_TREL_ROCKET_T))]
 
 trel_run_height_sim_iterations = TREL.trel_run_height_sim_iterations
-trel_run_height_sim_iterations.argtypes = [ctypes.POINTER(ctypes.POINTER(TREL_ROCKET))]
+trel_run_height_sim_iterations.argtypes = [ctypes.POINTER(ctypes.POINTER(_TREL_ROCKET_T))]
 
 _trel_grains_free = TREL.trel_grains_free
 _trel_grains_free.argtypes = [ctypes.POINTER(ctypes.POINTER(_TREL_GRAINS_T))]
@@ -198,97 +198,110 @@ _trel_engine_free = TREL.trel_engine_free
 _trel_engine_free.argtypes = [ctypes.POINTER(ctypes.POINTER((_TREL_ENGINE_T)))]
 _trel_engine_free.restype = ctypes.c_void_p
 
-trel_rocket_free = TREL.trel_rocket_free
-trel_rocket_free.argtypes = [ctypes.POINTER(ctypes.POINTER(TREL_ROCKET))]
-trel_rocket_free.restype = ctypes.c_void_p
+_trel_rocket_free = TREL.trel_rocket_free
+_trel_rocket_free.argtypes = [ctypes.POINTER(ctypes.POINTER(_TREL_ROCKET_T))]
+_trel_rocket_free.restype = ctypes.c_void_p
 
-def trel_print_rocket(rocket):
-    print("\nGrains data")
-    print("amount",                rocket.engine.contents.contents.grains.contents.contents.amount)
-    print("init_inter_radius",     rocket.engine.contents.contents.grains.contents.contents.init_inter_radius)
-    print("extern_radius",         rocket.engine.contents.contents.grains.contents.contents.extern_radius)
-    print("longitude",             rocket.engine.contents.contents.grains.contents.contents.longitude)
-    print("grain_separation",      rocket.engine.contents.contents.grains.contents.contents.grain_separation)
+class TREL_GRAINS():
+    def __init__(self, amount, internal_radius, external_radius, longitude, separation):
+        self.values = _TREL_GRAINS_T.from_address(_trel_grains_init(
+            amount, internal_radius, external_radius, longitude, separation))
+        self._p = ctypes.pointer(self.values)
+        self._p_p = ctypes.pointer(self._p)
+        self.allocated = True
+    def free(self):
+        _trel_grains_free(self._p_p)
+        self.allocated = False
+    def __repr__(self):
+        return('Amount: %f\nInternal radius: %f\nExternal radius: %f\nLongitude: %f\nSeparation: %f' % (
+            self.values.amount, self.values.init_inter_radius, self.values.extern_radius, self.values.longitude, self.values.grain_separation))
+    def __str__(self):
+        return(self.__repr__() if self.allocated else 'NULL')
 
-    print("\nFuel data")
-    print("const_burn_rate",       rocket.engine.contents.contents.fuel.contents.contents.const_burn_rate)
-    print("pressure_exponent",     rocket.engine.contents.contents.fuel.contents.contents.pressure_exponent)
-    print("density",               rocket.engine.contents.contents.fuel.contents.contents.density)
-    print("burn_rate",             rocket.engine.contents.contents.fuel.contents.contents.burn_rate)
+class TREL_FUEL():
+    def __init__(self, c_burn_rate, pressure_exponent, density):
+        self.values = _TREL_FUEL_T.from_address(_trel_fuel_init(
+            c_burn_rate, pressure_exponent, density))
+        self._p = ctypes.pointer(self.values)
+        self._p_p = ctypes.pointer(self._p)
+        self.allocated = True
+    def free(self):
+        _trel_fuel_free(self._p_p)
+        self.allocated = False
+    def __repr__(self):
+        return('Const Burn Rate: %f\nPressure Exponent: %f\nDensity: %f\nBurn Rate: %f' % (
+            self.values.const_burn_rate, self.values.pressure_exponent, self.values.density, self.values.burn_rate))
+    def __str__(self):
+        return(self.__repr__() if self.allocated else 'NULL')
 
-    print("\nScrews data")
-    print("diameter",              rocket.engine.contents.contents.screws.contents.contents.diameter)
-    print("dist_center_wall",      rocket.engine.contents.contents.screws.contents.contents.dist_center_wall)
-    print("area_per_screw",        rocket.engine.contents.contents.screws.contents.contents.area_per_screw)
-    print("screw_occupied_area",   rocket.engine.contents.contents.screws.contents.contents.screw_occupied_area)
-    print("width_cutting_segment", rocket.engine.contents.contents.screws.contents.contents.width_cutting_segment)
-    print("material",              rocket.engine.contents.contents.screws.contents.contents.material.decode('ASCII'))
-    print("amount",                rocket.engine.contents.contents.screws.contents.contents.amount)
+class TREL_SCREWS():
+    def __init__(self, material_name, amount, diameter, dist_center_wall):
+        if len(material_name) > 99:
+            print("ERROR: TREL's materials can't take more than 99 characters")
+            return(None)
+        self.values = _TREL_SCREWS_T.from_address(_trel_screws_init(
+            bytes(material_name, 'utf8'), amount, diameter, dist_center_wall))
+        self._p = ctypes.pointer(self.values)
+        self._p_p = ctypes.pointer(self._p)
+        self.allocated = True
+    def free(self):
+        _trel_screws_free(self._p_p)
+        self.allocated = False
+    def __repr__(self):
+        return('Material: %s\nAmount: %f\nDiameter: %f\nWall Center Distance: %f\nArea Per Screw:%f\nScrew Occupied Area: %f\nWidth Cutting Segment %f' % (
+        self.values.material, self.values.amount, self.values.diameter, self.values.dist_center_wall, self.values.area_per_screw, self.values.screw_occupied_area, self.values.width_cutting_segment))
+    def __str__(self):
+        return(self.__repr__() if self.allocated else 'NULL')
 
-    print("\nTubing data")
-    print("material",              rocket.engine.contents.contents.tube.contents.contents.material.decode('ASCII'))
-    print("diameter_ext",          rocket.engine.contents.contents.tube.contents.contents.diameter_ext)
-    print("wall_thickness",        rocket.engine.contents.contents.tube.contents.contents.wall_thickness)
-    print("internal_radius",       rocket.engine.contents.contents.tube.contents.contents.internal_radius)
-    print("young_module",          rocket.engine.contents.contents.tube.contents.contents.young_module)
-    print("sector_angle",          rocket.engine.contents.contents.tube.contents.contents.sector_angle)
-    print("mean_tubing_diameter",  rocket.engine.contents.contents.tube.contents.contents.mean_tubing_diameter)
-    print("shear_stress_tension",  rocket.engine.contents.contents.tube.contents.contents.shear_stress_tension)
-    print("shear_stress_pressure", rocket.engine.contents.contents.tube.contents.contents.shear_stress_pressure)
-    print("ult_stress_tension",    rocket.engine.contents.contents.tube.contents.contents.ult_stress_tension)
-    print("ult_stress_pressure",   rocket.engine.contents.contents.tube.contents.contents.ult_stress_pressure)
-    print("transversal_area",      rocket.engine.contents.contents.tube.contents.contents.transversal_area)
-    print("material_area",         rocket.engine.contents.contents.tube.contents.contents.material_area)
-    print("nozzle_efficiency",     rocket.engine.contents.contents.tube.contents.contents.nozzle_efficiency)
+class TREL_TUBING():
+    def __init__(self, material_name, external_diameter, thickness, young_module, shear_tension, shear_pressure, ult_tension, ult_pressure, nozzle_efficiency):
+        if len(material_name) > 99:
+            print("ERROR: TREL's materials can't take more than 99 characters")
+            return(None)
+        self.values = _TREL_TUBING_T.from_address(_trel_tubing_init(
+            bytes(material_name, 'utf8'), external_diameter, thickness, young_module, shear_tension, shear_pressure, ult_tension, ult_pressure, nozzle_efficiency))
+        self._p = ctypes.pointer(self.values)
+        self._p_p = ctypes.pointer(self._p)
+        self.allocated = True
+    def free(self):
+        _trel_tubing_free(self._p_p)
+        self.allocated = False
+    def __repr__(self):
+        return('Material: %s\nExternal Diameter: %f\nWall Thickness: %f\nInternal Radius: %f\nYoung Module: %f\nSector Angle: %f\nMean Tubing Diameter: %f\nShear Stress Tension: %f\nShear Stress Pressure: %f\nUltimate Stress Tension: %f\nUltimate Stress Pressure: %f\nTransversal Area: %f\nMaterial Area %f\nNozzle Efficiency %f' % (
+        self.values.material, self.values.diameter_ext, self.values.wall_thickness, self.values.internal_radius, self.values.young_module, self.values.sector_angle, self.values.mean_tubing_diameter, self.values.shear_stress_tension, self.values.shear_stress_pressure, self.values.ult_stress_tension, self.values.ult_stress_pressure, self.values.transversal_area, self.values.material_area, self.values.nozzle_efficiency))
+    def __str__(self):
+        return(self.__repr__() if self.allocated else 'NULL')
 
-    print("\nEngine data")
-    print("engine_mass",           rocket.engine.contents.contents.engine_mass)
-    print("pressure",              rocket.engine.contents.contents.pressure)
-    print("escape_vel",            rocket.engine.contents.contents.escape_vel)
-    print("temperature",           rocket.engine.contents.contents.temperature)
-    print("width_condition",       rocket.engine.contents.contents.width_condition)
-    print("margin_of_safety",      rocket.engine.contents.contents.margin_of_safety)
-    print("max_stress",            rocket.engine.contents.contents.max_stress)
-    print("radial_stress",         rocket.engine.contents.contents.radial_stress)
-    print("tangencial_stress",     rocket.engine.contents.contents.tangencial_stress)
-    print("longitudinal_stress",   rocket.engine.contents.contents.longitudinal_stress)
-    print("max_pressue",           rocket.engine.contents.contents.max_pressue)
-    print("nozzle_efficiency",     rocket.engine.contents.contents.nozzle_efficiency)
+class TREL_ENGINE():
+    def __init__(self, pressure, temperature, mass, grains, fuel, tubing, screws):
+        self.values = _TREL_ENGINE_T.from_address(_trel_engine_init(
+            pressure, temperature, mass, grains._p_p, fuel._p_p, tubing._p_p, screws._p_p))
+        self._p = ctypes.pointer(self.values)
+        self._p_p = ctypes.pointer(self._p)
+        self.allocated = True
+    def free(self):
+        _trel_engine_free(self._p_p)
+        self.allocated = False
+    def __repr__(self):
+        return('Engine Mass: %f\nPressue: %f\nEscape Velocity: %f\nTemperature %f\nWidth Condition: %f\nMargin of Safety: %f\nMaximum Stress: %f\nRadial Stress: %f\nTangencial Stress: %f\nLongitudinal Stress: %f\nMaximum Pressure: %f\nNozzle Efficiency: %f' % (
+        self.values.engine_mass, self.values.pressure, self.values.escape_vel, self.values.temperature, self.values.width_condition, self.values.margin_of_safety, self.values.max_stress, self.values.radial_stress, self.values.tangencial_stress, self.values.longitudinal_stress, self.values.max_pressue, self.values.nozzle_efficiency))
+    def __str__(self):
+        return(self.__repr__() if self.allocated else 'NULL')
 
-    print("\nComp area data")
-    print("avg_long_area",         rocket.engine.contents.contents.comp_area_values.contents.avg_long_area)
-    print("avg_trans_area",        rocket.engine.contents.contents.comp_area_values.contents.avg_trans_area)
-    print("avg_burn_area",         rocket.engine.contents.contents.comp_area_values.contents.avg_burn_area)
-    print("burn_std_deviation",    rocket.engine.contents.contents.comp_area_values.contents.burn_std_deviation)
-    print("burn_sum_diff",         rocket.engine.contents.contents.comp_area_values.contents.burn_sum_diff)
-
-    print("\nRocket data")
-    print("telemetry_mass",        rocket.telemetry_mass)
-    print("parachute_mass",        rocket.parachute_mass)
-    print("fuselage_mass",         rocket.fuselage_mass)
-    print("payload_mass",          rocket.payload_mass)
-    print("initial_height",        rocket.initial_height)
-    print("avg_thrust",            rocket.avg_thrust)
-    print("max_thrust",            rocket.max_thrust)
-    print("delta_v",               rocket.delta_v)
-    print("max_pressure",          rocket.max_pressure)
-    print("sim_latitude",          rocket.sim_latitude)
-    print("body_diameter",         rocket.body_diameter)
-    print("drag_coefficient",      rocket.drag_coefficient)
-
-    print("\nHeight sim data")
-    print("rocket_position list with %i items" %      (len(list(rocket.sim_values.contents.position))))
-    print("rocket_speed list with %i items" %         (len(list(rocket.sim_values.contents.speed))))
-    print("rocket_acceleration list with %i items" %  (len(list(rocket.sim_values.contents.acceleration))))
-    print("rocket_force_balance list with %i items" % (len(list(rocket.sim_values.contents.force_balance))))
-    print("rocket_drag list with %i items" %          (len(list(rocket.sim_values.contents.drag))))
-    print("rocket_weight list with %i items" %        (len(list(rocket.sim_values.contents.weight))))
-    print("rocket_mass list with %i items" %          (len(list(rocket.sim_values.contents.mass))))
-    print("rocket_force list with %i items" %         (len(list(rocket.sim_values.contents.force))))
-    print("time list with %i items" %                 (len(list(rocket.sim_values.contents.time))))
-    print("max_height",            rocket.sim_values.contents.max_height)
-    print("max_position",          rocket.sim_values.contents.max_position)
-    print("max_speed",             rocket.sim_values.contents.max_speed)
-    print("max_acceleration",      rocket.sim_values.contents.max_acceleration)
-    print("max_force_balance",     rocket.sim_values.contents.max_force_balance)
-    print("max_drag",              rocket.sim_values.contents.max_drag)
+class TREL_ROCKET():
+    def __init__(self, engine, telemetry_mass, parachute_mass, fuselage_mass, payload_mass, initial_height, sim_latitude, body_diameter, drag_coefficient):
+        self.values = _TREL_ROCKET_T.from_address(_trel_rocket_init(
+            engine._p_p, telemetry_mass, parachute_mass, fuselage_mass, payload_mass, initial_height, sim_latitude, body_diameter, drag_coefficient))
+        self._p = ctypes.pointer(self.values)
+        self._p_p = ctypes.pointer(self._p)
+        self.allocated = True
+    def free(self):
+        _trel_rocket_free(self._p_p)
+        self.allocated = False
+    def __repr__(self):
+        return('Telemetry Mass: %f\nParachute Mass: %f\nFuselage Mass: %f\nPayload Mass: %f\nInitial Height: %f\nAverage Thrust: %f\nMaximum Thrust: %f\nDelta V: %f\nMaximum Pressure: %f\nSimulation Latitude: %f\nBody Diameter: %f\nDrag Coefficient: %f' % (
+        self.values.telemetry_mass, self.values.parachute_mass, self.values.fuselage_mass, self.values.payload_mass, self.values.initial_height, self.values.avg_thrust, self.values.max_thrust, self.values.delta_v, self.values.max_pressure, self.values.sim_latitude, self.values.body_diameter, self.values.drag_coefficient))
+    def get_sim_values(self):
+        return(self.values.sim_values.contents)
+    def __str__(self):
+        return(self.__repr__() if self.allocated else 'NULL')
--- Interfaces/Python/highmanualtest.py
+++ Interfaces/Python/highmanualtest.py
@@ -1,4 +1,4 @@
-from HIGH_TREL import *
+from TREL import *
 
 engine_grains = TREL_GRAINS(
     4,
@@ -38,15 +38,29 @@ engine = TREL_ENGINE(
     engine_tubing,
     engine_screws
 )
+rocket = TREL_ROCKET(
+    engine,
+    0.3,
+    0.05,
+    5.0,
+    1.5,
+    1207.0,
+    9.936111,
+    0.13,
+    0.4
+)
 
 print(f'Grains\n{engine_grains}')
 print(f'Fuel\n{engine_fuel}')
 print(f'Screws\n{engine_screws}')
 print(f'Tubing\n{engine_tubing}')
 print(f'Engine\n{engine}')
+print(f'Rocket\n{rocket}')
+print(list(rocket.get_sim_values().position))
 
 engine_grains.free()
 engine_fuel.free()
 engine_screws.free()
 engine_tubing.free()
 engine.free()
+rocket.free()
